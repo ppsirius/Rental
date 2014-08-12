bfa07082e2bd8fbda926a9c080ba8ff7f5cc5e40
Jakub Nowakowski <jakubnowakowski86@gmail.com>
2014-08-12 14:04:04 +0200

    Brand validation polished

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,3 @@
 class Brand < ActiveRecord::Base
-  validates :brand_name, presence: true, length: (minimum: 2), format: {with: /\A[a-zA-Z]+\z/}
+  validates :brand_name, presence: true, length: {minimum: 3}, format: {with: /\A[a-zA-Z]+\z/}
 end
