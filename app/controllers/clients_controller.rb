0296cf2a909117d1deeff50e76096d79836154a1
Piotr Pupczyk <piotrek@jakbyco.com>
2014-08-12 16:39:41 +0200

    add client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,6 @@
 class ClientsController < ApplicationController
   def index
-  end
-
-  def new
+  	@clients = Client.all
   end
 
   def show
@@ -10,4 +8,42 @@ class ClientsController < ApplicationController
 
   def edit
   end
+
+  def new
+  	@client = Client.new
+  end
+
+  def create
+  	@client = Client.new(client_params)
+  		if @client.save
+  			redirect_to @client, notice: 'Client was successfully created.'
+  		else
+  			render :new
+  		end
+  end
+
+	def update
+      if @client.update(client_params)
+        redirect_to @client, notice: 'Client was successfully updated.' 
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @client.destroy
+      redirect_to clients_url, notice: 'Client was successfully destroyed.'
+    end
+
+private
+
+	def set_client
+		@client = Client.find(params[:id])
+	end
+
+	def client_params
+		params.require(:client).permit(:name, :surnam, :age, :document_no, :document_type)
+	end
+
 end
+
