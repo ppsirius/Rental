43336547bf5459d39c8b954a653cf5d52c0f1e91
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-12 16:48:12 +0200

    Some more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,8 @@ Client.create(name: 'John', surname: 'Doe', age: 23, document_no: 'AGF1234', doc
 Client.create(name: 'Tom', surname: 'Flo', age: 44, document_no: 'AGF9999', document_type: 'ID')
 Client.create(name: 'Ben', surname: 'Man', age: 34, document_no: 'ZZZ1234', document_type: 'ID')
 
+Car.create(model_id: 2, mileage: 2000, registration_no: "KR1234", year_of_manufacture: 2000)
+Car.create(model_id: 3, mileage: 23000, registration_no: "WW1234", year_of_manufacture: 2012)
 
+Rent.create(client_id: 1, car_id: 2)
+Rent.create(client_id: 2, car_id: 1)
