f9a2018aa54c29ae3a8f115ccccf0e562cc7384e
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-12 14:54:41 +0200

    Client controller add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'clients/index'
+
+  get 'clients/new'
+
+  get 'clients/show'
+
+  get 'clients/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
