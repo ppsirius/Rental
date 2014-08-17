7925366ecb838f987787f1a80fc19215075e0bda
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-17 21:44:45 +0200

    Controllers and form for Car model add

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,6 +5,31 @@ class CarsController < ApplicationController
     @cars = Car.all
   end
 
+  def show
+  end
 
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(contact_params)
+    if @car.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:milage, :registration_no, :year_of_manufactur, :model_id)
+  end
 
 end
